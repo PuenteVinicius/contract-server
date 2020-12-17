f98729401c1e9205f68da4e68b54127674c185f6
vinicius Puente <vinicius.puente@db1.com.br>
2020-12-17 11:50:03 -0300

    adiciona estrutura para todos os Metodos HTTP

--- index.js
+++ index.js
@@ -16,55 +16,64 @@ init();
 
 function init() {
   fs.readdir(dir, (err, items) => {
-    if(err) {
-      console.log('err', errr);
+    if (err) {
+      console.log("err", errr);
     }
     items.forEach((file) => {
-      fs.readFile(dir + file, 'utf8' , (err, contract) => {
-        if(err) {
-          console.log('err', errr);
+      fs.readFile(dir + file, "utf8", (err, contract) => {
+        if (err) {
+          console.log("err", errr);
         }
         return createRequest(contract);
       });
-    })
-  })
+    });
+  });
 }
+
+function requests() {
+  return {
+    GET: GET,
+    POST: POST,
+    PUT: PUT,
+    DELETE: DELETE,
+  };
+}
+
 
 function createRequest(data) {
   const contract = JSON.parse(data);
-  if(contract.method === 'GET') {
-    server.get(contract.path, (request, response) => {
-      if(request.method === 'GET') {
-        const data = contract.response;
-        return response.status(200).json(data);
-      }
-    })
-  }
-  if(contract.method === 'POST') {
-    server.get(contract.path, (request, response) => {
-      if(request.method === 'POST') {
-        const data = contract.body;
-        return response.status(200).json(data);
-      }
-    })
-  }
-  if(contract.method === 'PUT') {
-    server.get(contract.path, (request, response) => {
-      if(request.method === 'PUT') {
-        const data = contract.body;
-        return response.status(200).json(data);
-      }
-    })
-  }
-  if(contract.method === 'DELETE') {
-    server.get(contract.path, (request, response) => {
-      if(request.method === 'DELETE') {
-        const data = contract.body;
-        return response.status(200).json(data);
-      }
-    })
-  }
+
+  requests()[contract.method](contract);
 }
-  
 
+function GET(contract) {
+  server.get(contract.path, (response) => {
+    return response.status(200).json(contract.response);
+  });
+}
 
+function POST(contract) {
+  server.post(contract.path, (request, response) => {
+    resquestMount(contract, request, response);
+  });
+}
+
+function PUT(contract) {
+  server.put(contract.path, (request, response) => {
+    resquestMount(contract, request, response);
+  });
+}
+
+function DELETE(contract) {
+  server.delete(contract.path, (req, response) => {
+    return response.send({success: true});
+  }); 
+}
+
+function resquestMount(contract, request, response) {
+  let data = contract.response;
+  if(JSON.stringify(request.body) == JSON.stringify(contract.body)) {
+    return response.status(200).json(data);
+  }
+  return response.status(400).json('Wrong Object');
+}
